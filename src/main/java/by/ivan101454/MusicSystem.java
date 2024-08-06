86d8348993694ef25c207a366fbce561315b9c8d
Ivan Sapozhnikov <ivan101454@gmail.com>
2024-08-06 19:59:17 +0300

    add method volume in class MusicSystem

--- src/main/java/by/ivan101454/MusicSystem.java
+++ src/main/java/by/ivan101454/MusicSystem.java
@@ -3,6 +3,10 @@ package by.ivan101454;
 public class MusicSystem {
     private String model;
     private Double power;
+    private int Volume;
+    private final int maxVolume = 100;
+    private final int minVolume = 0;
+
 
     public MusicSystem(String model, Double power) {
         this.model = model;
@@ -23,5 +27,13 @@ public class MusicSystem {
 
     public void setPower(Double power) {
         this.power = power;
+    }
+
+    public int getVolume() {
+        return Volume;
+    }
+
+    public void setVolume(int volume) {
+        Volume = volume;
     }
 }
