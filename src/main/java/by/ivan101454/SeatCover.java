6599453a0f4d1c16fc3d293beed95bda34fe6219
Ivan Sapozhnikov <ivan101454@gmail.com>
2024-08-06 17:12:18 +0300

    add class SeatCover

---
+++ src/main/java/by/ivan101454/SeatCover.java
@@ -0,0 +1,27 @@
+package by.ivan101454;
+
+public class SeatCover {
+    private String color;
+    private String material;
+
+    public SeatCover(String color, String material) {
+        this.color = color;
+        this.material = material;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getMaterial() {
+        return material;
+    }
+
+    public void setMaterial(String material) {
+        this.material = material;
+    }
+}
