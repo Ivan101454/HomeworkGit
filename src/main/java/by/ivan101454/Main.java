2be24ee50d4e30d21578bf3cfbf4079e1e664620
Ivan Sapozhnikov <ivan101454@gmail.com>
2024-08-06 22:31:07 +0300

    add method go

--- src/main/java/by/ivan101454/Main.java
+++ src/main/java/by/ivan101454/Main.java
@@ -2,7 +2,7 @@ package by.ivan101454;
 
 public class Main {
     public static void main(String[] args) {
-        Car car = new Car();
+        Car car = new Car("VW", new Engine(1.8, "AWT", Fuel.GAS), new FuelTank());
         System.out.println(car.toString());
     }
 }
