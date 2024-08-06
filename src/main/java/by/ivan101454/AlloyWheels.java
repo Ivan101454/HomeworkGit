2ffe3a3d2445c288b0a2dbcca604eb29c3f9a383
Ivan Sapozhnikov <ivan101454@gmail.com>
2024-08-06 18:00:03 +0300

    add class AlloyWheels

---
+++ src/main/java/by/ivan101454/AlloyWheels.java
@@ -0,0 +1,27 @@
+package by.ivan101454;
+
+public class AlloyWheels {
+    private String brand;
+    private int radius;
+
+    public AlloyWheels(String brand, int radius) {
+        this.brand = brand;
+        this.radius = radius;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public int getRadius() {
+        return radius;
+    }
+
+    public void setRadius(int radius) {
+        this.radius = radius;
+    }
+}
